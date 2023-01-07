23102c5e913345aa7ee6c1c1fe91a7520c685dd2
Ei187 <87334398+Ei187@users.noreply.github.com>
2023-01-07 22:43:35 +0200

    Update libvtcapture.cpp

--- src/backends/libvtcapture.cpp
+++ src/backends/libvtcapture.cpp
@@ -51,13 +51,13 @@ int capture_init(cap_backend_config_t* config, void** state_p)
     INFO("Driver created!");
 
     // Sorry, no unlimited fps for you.
-    self->props.frm = config->fps == 0 ? 120 : config->fps;
-    self->props.dump = 4;
+    self->props.frm = config->fps == 0 ? 60 : config->fps;
+    self->props.dump = -1;
     self->props.loc.x = 0;
     self->props.loc.y = 0;
     self->props.reg.w = config->resolution_width;
     self->props.reg.h = config->resolution_height;
-    self->props.buf_cnt = 3;
+    self->props.buf_cnt = 2;
 
     if (HAS_QUIRK(config->quirks, QUIRK_VTCAPTURE_FORCE_CAPTURE)) {
         self->quirk_force_capture = true;
